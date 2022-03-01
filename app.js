2b96584d7b630edbaa12fa9b2dd37ac49c1eb9c1
Hovhannes Kocharyan <hovkocharyan@yandex.com>
2022-03-01 23:19:44 +0400

    feat: added config for localhost and express

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
+const express = require("express");
+const config = require("config");
+const app = express();
+
+const PORT = config.get("port") || 3000;
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+
 console.log("App.js is running!");
